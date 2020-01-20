a830f3968d1d1d8f22d4ee202addcf4cc55a62e2
Jorge Jacobo <jorge.jacobo.francisco.306@gmail.com>
2020-01-19 22:57:35 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recapcha';
+
+  myRecaptcha = new FormControl(false);
+ 
+    onScriptLoad() {
+        console.log('Google reCAPTCHA loaded and is ready for use!')
+    }
+ 
+    onScriptError() {
+        console.log('Something went long when loading the Google reCAPTCHA')
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { RecaptchaModule } from 'angular-google-recaptcha';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RecaptchaModule.forRoot({
+      siteKey: '6LdGqNAUAAAAAL7qwP4DmbVnlxzYDGE1VAJOOOZS',
+  }),
   ],
   providers: [],
   bootstrap: [AppComponent]
